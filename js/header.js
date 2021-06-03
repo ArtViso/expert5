286ece1bf67e04d6dcd30c9459903fb64769a155
Nikita <nbokii18@gmail.com>
2021-06-03 20:04:02 +0300

    thanks_new_script

--- js/header.js
+++ js/header.js
@@ -68,27 +68,3 @@ li1.addEventListener('mouseover',()=>{
   })
 
   //for thanks.html
-  if(document.querySelector('.block__tell__attachment_no_margin_top')) {
-    const burger = document.querySelector('.burger');
-    const clos = document.querySelector('.close');
-    const chevron = document.querySelector('.chevron');
-    const header = document.querySelector('.header');
-    const section = document.querySelector('section');
-    const headerMain = document.querySelector('.header__main');
-    burger.addEventListener('click',()=>{
-      chevron.style.display = 'block';
-      burger.style.display = 'none';
-      clos.style.display = 'block';
-      header.style.display = 'none';
-      section.style.display = 'none';
-      headerMain.style.display = 'none';
-    });
-    clos.addEventListener('click',()=>{
-      chevron.style.display = 'none';
-      burger.style.display = 'block';
-      clos.style.display = 'none';
-      header.style.display='block';
-      section.style.display = 'block';
-      headerMain.style.display = 'block';     
-  });
-  }
