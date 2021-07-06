771f126df37cb1b1d327c367dc7e507ce01e3645
Nikita <nbokii18@gmail.com>
2021-07-06 23:23:25 +0300

    choose_modal

---
+++ js/choose_modal.js
@@ -0,0 +1,44 @@
+const choosebtns =  document.querySelector('.btns');
+const choosebtn2 = document.querySelector('.btn2');
+
+
+const chooseModallBlock = document.querySelector('.call__master__modal');
+const chooseClosedModal = document.querySelector('.call__master__modal .closedModal');
+
+const [choose_left_block, choose_right_block] = document.querySelectorAll('.choose__call__item__block');
+
+const showModalChoose = () => { 
+    if(window.innerWidth > 651){
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100%";
+    chooseModallBlock.style.top = document.documentElement.scrollTop - 20 +'px';
+    chooseModallBlock.style.display = 'flex';  
+    } 
+}
+
+chooseModallBlock.addEventListener('click',(e)=>{
+    if(e.target === chooseModallBlock || e.target === chooseClosedModal){
+    chooseModallBlock.style.display = 'none';
+    document.body.style.overflow = "auto";
+    document.body.style.height = "auto";
+    }
+});
+
+choosebtns.addEventListener('click', showModalChoose);
+choosebtn2.addEventListener('click', showModalChoose);
+choose_left_block.addEventListener('mouseenter', chooseOnMouseEnter);
+choose_left_block.addEventListener('mouseleave', chooseOnMouseLeave);
+choose_right_block.addEventListener('mouseenter', chooseOnMouseEnter);
+choose_right_block.addEventListener('mouseleave', chooseOnMouseLeave);
+
+function chooseOnMouseEnter(){
+    this.style.backgroundColor = "#1117A3";
+    this.querySelector('.default').style.display='none';
+    this.querySelector('.white').style.display='block';
+}
+
+function chooseOnMouseLeave(){
+    this.style.backgroundColor = "#F3F4F9";
+    this.querySelector('.white').style.display='none';
+    this.querySelector('.default').style.display='block';
+}
