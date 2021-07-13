5388eab4ed42a3b0a2447ad4c7cd694bb11b8a06
Nikita <nbokii18@gmail.com>
2021-07-13 19:56:28 +0300

    all modals (no style)

--- js/choose_modal.js
+++ js/choose_modal.js
@@ -6,6 +6,16 @@ const chooseModallBlock = document.querySelector('.call__master__modal');
 const chooseClosedModal = document.querySelector('.call__master__modal .closedModal');
 const chooseModalContent =  document.querySelector('.call__master__modal__content');
 const urgentCallModal = document.querySelector('.call__master__urgent');
+const nonUrgentCallModalFirst = document.querySelector('.call__master__nonurgent__first');
+const nonUrgentCallModalSecond = document.querySelector('.call__master__nonurgent__second');
+const payModal = document.querySelector('.call__master__pay');
+
+const nonUrgentCallNextButton = document.querySelector('.call__master__nonurgent__first__btn');
+const nonUrgentCallPrevButton = document.querySelector('.call__master__nonurgent__second__btns .prev__btn');
+const urgentPayButton = document.querySelector('.call__master__urgent__btn');
+const nonUrgentPayButton = document.querySelector('.call__master__nonurgent__second__btns .pay__btn');
+
+
 
 const [choose_left_block, choose_right_block] = document.querySelectorAll('.choose__call__item__block');
 
@@ -25,6 +35,9 @@ chooseModallBlock.addEventListener('click',(e)=>{
     document.body.style.height = "auto";
     chooseModalContent.style.display = 'block';
     urgentCallModal.style.display = 'none';
+    nonUrgentCallModalFirst.style.display = 'none';
+    nonUrgentCallModalSecond.style.display = 'none';
+    payModal.style.display = 'none';
     }
 });
 
@@ -33,14 +46,40 @@ const showModalUrgentCall = () => {
     urgentCallModal.style.display = 'block';
 }
 
+const showModalNonUrgentCallFirst = () => {
+    chooseModalContent.style.display = 'none';
+    nonUrgentCallModalFirst.style.display = 'block';
+}
+
+const showModalNonUrgentCallSecond = () => {
+    nonUrgentCallModalFirst.style.display = 'none';
+    nonUrgentCallModalSecond.style.display = 'block';
+}
+
+const showModalNonUrgentCallFirstFromSecond = () => {
+    nonUrgentCallModalSecond.style.display = 'none';
+    nonUrgentCallModalFirst.style.display = 'block';
+}
+
+const showPayModal = () => {
+    nonUrgentCallModalSecond.style.display = 'none';
+    urgentCallModal.style.display = 'none';
+    payModal.style.display = 'block';
+}
 
 choosebtns.addEventListener('click', showModalChoose);
 choosebtn2.addEventListener('click', showModalChoose);
 choose_left_block.addEventListener('mouseenter', chooseOnMouseEnter);
 choose_left_block.addEventListener('mouseleave', chooseOnMouseLeave);
 choose_left_block.addEventListener('click', showModalUrgentCall);
+choose_right_block.addEventListener('click', showModalNonUrgentCallFirst);
 choose_right_block.addEventListener('mouseenter', chooseOnMouseEnter);
 choose_right_block.addEventListener('mouseleave', chooseOnMouseLeave);
+nonUrgentCallNextButton.addEventListener('click', showModalNonUrgentCallSecond);
+nonUrgentCallPrevButton.addEventListener('click', showModalNonUrgentCallFirstFromSecond);
+urgentPayButton.addEventListener('click', showPayModal);
+nonUrgentPayButton.addEventListener('click', showPayModal);
+
 
 function chooseOnMouseEnter(){
     this.style.backgroundColor = "#1117A3";
