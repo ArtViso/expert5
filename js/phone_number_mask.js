8410bbc722ef30bdaae69f12cda58a924cb53545
Nikita <nbokii18@gmail.com>
2021-07-14 21:01:47 +0300

    script little fix (phone mask)

--- js/phone_number_mask.js
+++ js/phone_number_mask.js
@@ -2,11 +2,23 @@ const pnm_phones = {
     'ua': '+380 (99) 999-99-99',
     'ru': '+7 (999) 999-99-99'
 };
-const pnm_select = document.querySelector('.call__master__modal .call__master__urgent .call__master__urgent__phone select');
-const pnm_input = document.querySelector('.call__master__modal .call__master__urgent .call__master__urgent__phone input[type=text]');
-pnm_select.addEventListener('change', e=>{
-    pnm_input.value = '';
-    pnm_input.placeholder = pnm_phones[e.target.value];
-    $(pnm_input).mask(pnm_phones[e.target.value]);
+
+const u_select = document.querySelector('.call__master__modal .call__master__urgent .call__master__urgent__phone select');
+const u_input = document.querySelector('.call__master__modal .call__master__urgent .call__master__urgent__phone input[type=text]');
+
+const n_select = document.querySelector('.call__master__modal .call__master__nonurgent__second .call__master__nonurgent__second__phone select');
+const n_input = document.querySelector('.call__master__modal .call__master__nonurgent__second .call__master__nonurgent__second__phone input[type=text]');
+
+u_select.addEventListener('change', e=>{
+    u_input.value = '';
+    u_input.placeholder = pnm_phones[e.target.value];
+    $(u_input).mask(pnm_phones[e.target.value]);
 });
-$(pnm_input).mask(pnm_phones['ua']);
+$(u_input).mask(pnm_phones['ua']);
+
+n_select.addEventListener('change', e=>{
+    n_input.value = '';
+    n_input.placeholder = pnm_phones[e.target.value];
+    $(n_input).mask(pnm_phones[e.target.value]);
+});
+$(n_input).mask(pnm_phones['ua']);
